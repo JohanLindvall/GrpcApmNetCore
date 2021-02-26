cca8687abd36332bcb855483af501d78c78077a3
Johan Lindvall <johan.lindvall@combination.se>
2021-02-26 16:31:04 +0100

    Initial

---
+++ GrpcApm/Services/GreeterService.cs
@@ -0,0 +1,30 @@
+using Grpc.Core;
+using Grpc.Net.Client;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace GrpcApm
+{
+    public class GreeterService : Greeter.GreeterBase
+    {
+        private readonly Greeter.GreeterClient client;
+
+        public GreeterService()
+        {
+            client = new Greeter.GreeterClient(GrpcChannel.ForAddress("http://localhost:5000"));
+        }
+
+        public override async Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return await client.SayHello2Async(request);
+        }
+
+        public override Task<HelloReply> SayHello2(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply
+            {
+                Message = "Hello " + request.Name
+            });
+        }
+    }
+}
